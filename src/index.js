b8c30e73567f51bf44fecbfeaca3fbf54410a393
julianostroschon <julianostroschon@gmail.com>
2024-04-11 15:54:52 -0300

    ➕ Configurações iniciais

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
   console.log({ body: request.body });
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  return new Response("Hello worker!", {
+    headers: { "content-type": "text/plain" }
+  });
 }
